71345f9cae6f740346499fb3af0afbf8b81f9d0f
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-02-15 15:15:33 -0400

    first commit

---
+++ 2401-todo-demo/src/ToDoList.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function ToDoList() {
+  return (
+    <>
+      <h2>My To Do List</h2>
+      <ul>
+        <li>Teach React</li>
+        <li>React Demo</li>
+      </ul>
+    </>
+  );
+}
+
+export default ToDoList;
